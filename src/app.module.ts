f2082345cb6ad36a1a42076cc8d006b972328a95
unknown <helodahlke@hotmail.com>
2024-01-10 23:21:29 -0300

    importing modules into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'orm.config';
 import { UserModule } from './user/user.module';
 import { FriendshipModule } from './friendship/friendship.module';
+import { ConversationModule } from './conversation/conversation.module';
+import { MessageModule } from './message/message.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
     UserModule,
-    FriendshipModule
+    FriendshipModule,
+    ConversationModule,
+    MessageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
